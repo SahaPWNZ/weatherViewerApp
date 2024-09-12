96844770fe0813fa0d7222aec25ef8f6432e6098
SahaPWNZ <007saha@inbox.ru>
2024-09-12 17:26:34 +0300

    fix: CookiesFilter bug add: logging в CookiesFilter, AppListener

--- src/main/java/com/example/weatherviewerapp/filters/CookiesFilter.java
+++ src/main/java/com/example/weatherviewerapp/filters/CookiesFilter.java
@@ -8,23 +8,29 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 
+@Slf4j
 @WebFilter(urlPatterns = {"/index.html"})
 public class CookiesFilter extends HttpFilter {
     private final CookieService cookieService = new CookieService();
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_GREEN = "\u001B[32m";
 
     @Override
     public void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
-
+        log.info(ANSI_GREEN + "Запуск фильтра Куки" + ANSI_RESET);
         Cookie cookie = cookieService.getCookie(req);
 
         if (cookie == null || !cookieService.isCookieInDB(cookie)) {
+            log.info(ANSI_GREEN + "Нужных куки нет, перенаправление на sign-in" + ANSI_RESET);
             res.sendRedirect("sign-in.html");
         } else {
+            log.info(ANSI_GREEN + "Куки есть - обновляю время жизни куки" + ANSI_RESET);
             cookieService.updateUserSession(cookie);
-            doFilter(req, res, chain);
+            super.doFilter(req, res, chain);
 
         }
     }
--- src/main/java/com/example/weatherviewerapp/listner/AppListener.java
+++ src/main/java/com/example/weatherviewerapp/listner/AppListener.java
@@ -4,13 +4,16 @@ import com.example.weatherviewerapp.utils.Scheduler;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @WebListener
 public class AppListener implements ServletContextListener {
     private final Scheduler scheduler = new Scheduler();
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
+        log.info("Запуск шедулера в листнере");
         scheduler.startCleanUpTusk();
     }
 
